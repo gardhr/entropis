5755f7820ba8215612eaca590c1726458da03a27
gardhr <24468810+gardhr@users.noreply.github.com>
2023-02-14 09:57:08 -0800

    not yet browser-compatible

--- demo.js
+++ demo.js
@@ -1,42 +1,38 @@
 const entropis = require("./entropis");
+const log = console.log;
 let argv = process.argv.slice(1);
 let argc = argv.length;
-if (argc <= 1) console.log("Usage:", argv[0], "[PASS] [SALT] [DIGITS]");
+if (argc <= 1) log("Usage:", argv[0], "[PASS] [SALT] [DIGITS]");
 let pass;
 if (argc > 1) pass = argv[1];
 let salt;
 if (argc > 2) salt = argv[2];
 let digits;
 if (argc > 3) digits = Number(argv[3]);
-console.log(
-  "entropis.hash(pass, salt, digits):",
-  entropis.hash(pass, salt, digits)
-);
+log("entropis.hash(pass, salt, digits):", entropis.hash(pass, salt, digits));
 var encoded = entropis.encode(pass, argv[0]);
-console.log("entropis.encode(pass, argv[0]):", encoded);
-console.log("entropis.decode(pass, encoded):", entropis.decode(pass, encoded));
+log("entropis.encode(pass, argv[0]):", encoded);
+log("entropis.decode(pass, encoded):", entropis.decode(pass, encoded));
 var domain = "example.com";
 var another = "MyReallyStrongPassword#923333";
-console.log(
+log(
   "entropis.set(pass, domain, another):",
   entropis.set(pass, domain, another)
 );
-console.log("entropis.get('foo', domain):", entropis.get("foo", domain));
-console.log("entropis.get(pass, domain):", entropis.get(pass, domain));
-console.log("entropis.remove(pass, domain):", entropis.remove(pass, domain));
-console.log("entropis.get(pass, domain):", entropis.get(pass, domain));
-console.log(
+log("entropis.get('foo', domain):", entropis.get("foo", domain));
+log("entropis.get(pass, domain):", entropis.get(pass, domain));
+log("entropis.remove(pass, domain):", entropis.remove(pass, domain));
+log("entropis.get(pass, domain):", entropis.get(pass, domain));
+log(
   "entropis.set(pass, domain, another):",
   entropis.set(pass, domain, another)
 );
-console.log(
+log(
   "entropis.set('foo', domain, another):",
   entropis.set("foo", domain, another)
 );
-console.log("entropis.get(pass, domain):", entropis.get(pass, domain));
+log("entropis.get(pass, domain):", entropis.get(pass, domain));
 var newpass = "!@ChurchillWa$NoNovice";
-console.log("entropis.change(pass, domain):", entropis.change(pass, newpass));
-console.log("entropis.get(newpass, domain):", entropis.get(newpass, domain));
-for(var i = 0; i < 500; ++i)
- entropis.hash(pass)
-
+log("entropis.change(pass, domain):", entropis.change(pass, newpass));
+log("entropis.get(newpass, domain):", entropis.get(newpass, domain));
+//for (var i = 0; i < 500; ++i) entropis.hash(pass);
